638ddb244fc9818414b96adc344205ba61a2407a
Vitaly Kuznetsov <vitty@redhat.com>
2013-12-02 10:29:32 +0100

    smportal separated

--- stageportal/__init__.py
+++ stageportal/__init__.py
@@ -1,4 +1,5 @@
-from stageportal import StagePortal, StagePortalException
+from baseportal import BasePortal, BasePortalException
+from smportal import SMPortal, SMPortalException
 from rhnclassic import RhnClassicPortal, RhnClassicPortalException
 
-__all__ = ['StagePortal', 'StagePortalException', 'RhnClassicPortal', 'RhnClassicPortalException']
+__all__ = ['BasePortal', 'BasePortalException', 'SMPortal', 'SMPortalException', 'RhnClassicPortal', 'RhnClassicPortalException']
